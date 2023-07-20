288130c4ef58f3b2bc810f9811417d793ba5dc72
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2023-07-20 22:16:03 +0800

    Create Layout
    feat: add layout of APIs
    
    fix: fix missing singleton function

---
+++ src/components/utils.py
@@ -0,0 +1,17 @@
+from functools import wraps
+
+
+def singleton(cls):
+    """
+    :param cls: cls
+    :return: instance
+    """
+    _instances = {}
+
+    @wraps(cls)
+    def instance(*args, **kw):
+        if cls not in _instances:
+            _instances[cls] = cls(*args, **kw)
+        return _instances[cls]
+
+    return instance
