7b3e37084803b11030ef9758655f35bfe2b6aac5
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2024-01-16 22:00:06 +0800

    Fix dependency bug
    fix: fix sanic-ext version

--- src/components/config.py
+++ src/components/config.py
@@ -12,7 +12,7 @@ from loguru import logger
 from pydantic import BaseModel
 from vyper import Vyper
 
-CONFIG_BUILD_VERSION = "v0.0.7"
+CONFIG_BUILD_VERSION = "v0.0.8"
 CONFIG_HOME_PATH = os.path.expanduser('~')
 CONFIG_CONFIG_NAME = "apiserver"
 CONFIG_PROJECT_NAME = "clpl"
